6dfaab8ecc69fa1a8942070a0424c3a788216f98
Tom McFarlane <tom.mcfarlane@gear4music.com>
2020-10-23 22:19:35 +0100

    Implemented teapot exception for PHPStans sanity

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Exceptions\ImANumptyException;
 use App\Exceptions\RequestException;
 use Psr\Http\Message\RequestInterface;
 
@@ -10,8 +11,13 @@ abstract class AbstractController
     protected const JSON = 'application/json';
     protected const ACCEPTED = 200;
     protected const BAD_REQUEST = 400;
+    protected const UNSUPPORTED_MIME_TYPE = 415;
+    protected const TEA_POT = 418;
+    protected const UNPROCESSABLE_ENTITY = 422;
     protected const CREATED = 201;
     protected const INTERNAL_SERVER_ERROR = 500;
+    private const HEADER_CONTENT_TYPE = 'Content-type';
+    private const HEADER_ACCEPT = 'Accept';
 
     /**
      * Validates is JSON request
@@ -20,16 +26,36 @@ abstract class AbstractController
      */
     protected function validateRequest(RequestInterface $request): void
     {
-        $contentType = $request->getHeader('Content-type');
+        $contentType = $request->getHeader(self::HEADER_CONTENT_TYPE);
         if (!empty($contentType[0]) && !str_contains($contentType[0], self::JSON)
             && !empty($request->getBody()->getContents())
         ) {
-            throw new RequestException('Must be type JSON');
+            throw new RequestException(
+                sprintf('Request header "%s" must be type "%s"', self::HEADER_CONTENT_TYPE, self::JSON),
+                self::UNSUPPORTED_MIME_TYPE
+            );
         }
 
-        $acceptHeader = $request->getHeader('Accept');
+        $acceptHeader = $request->getHeader(self::HEADER_ACCEPT);
         if (!empty($acceptHeader[0]) && !str_contains($acceptHeader[0], self::JSON)) {
-            throw new RequestException('Must be type JSON');
+            throw new RequestException(
+                sprintf('Request header "%s" must be type "%s"', self::HEADER_ACCEPT, self::JSON),
+                self::BAD_REQUEST
+            );
         }
+    }
+
+    /**
+     * @param string[] $message
+     * @return string
+     * @throws ImANumptyException
+     */
+    protected static function jsonEncodeArray(array $message): string
+    {
+        $message = json_encode($message);
+        if ($message === false) {
+            throw new ImANumptyException('Can you even code bro', self::TEA_POT);
+        }
+        return $message;
     }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,38 +2,96 @@
 
 namespace App\Controller;
 
+use App\Exceptions\ImANumptyException;
+use App\Exceptions\RequestException;
+use App\Service\UserService;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Throwable;
 
 class UserController extends AbstractController
 {
+    private UserService $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @return ResponseInterface
+     * @throws ImANumptyException
+     */
     public function getAll(RequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $response->getBody()->write(json_encode(['message' => '@todo - configure ' . __METHOD__]));
+        try {
+            $this->validateRequest($request);
+            $this->userService->getAllUsers();
+        } catch (RequestException $exception) {
+            $response->getBody()->write(self::jsonEncodeArray(['message' => $exception->getMessage()]));
+            return $response->withStatus($exception->getCode());
+        } catch (ImANumptyException $exception) {
+            $response->getBody()->write(self::jsonEncodeArray(['message' => $exception->getMessage()]));
+            return $response->withStatus($exception->getCode());
+        } catch (Throwable $exception) {
+            $response->getBody()->write(self::jsonEncodeArray(['message' => $exception->getMessage()]));
+            return $response->withStatus(self::INTERNAL_SERVER_ERROR);
+        }
+        $response->getBody()->write(self::jsonEncodeArray(['message' => '@todo - configure ' . __METHOD__]));
         return $response->withStatus(self::ACCEPTED);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param string[] $args
+     * @return ResponseInterface
+     * @throws ImANumptyException
+     */
     public function getUser(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $response->getBody()->write(json_encode(['message' => '@todo - configure ' . __METHOD__]));
+        $response->getBody()->write(self::jsonEncodeArray(['message' => '@todo - configure ' . __METHOD__]));
         return $response->withStatus(self::ACCEPTED);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param string[] $args
+     * @return ResponseInterface
+     * @throws ImANumptyException
+     */
     public function createUser(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $response->getBody()->write(json_encode(['message' => '@todo - configure ' . __METHOD__]));
+        $response->getBody()->write(self::jsonEncodeArray(['message' => '@todo - configure ' . __METHOD__]));
         return $response->withStatus(self::ACCEPTED);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param string[] $args
+     * @return ResponseInterface
+     * @throws ImANumptyException
+     */
     public function updateUser(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $response->getBody()->write(json_encode(['message' => '@todo - configure ' . __METHOD__]));
+        $response->getBody()->write(self::jsonEncodeArray(['message' => '@todo - configure ' . __METHOD__]));
         return $response->withStatus(self::ACCEPTED);
     }
 
+    /**
+     * @param RequestInterface $request
+     * @param ResponseInterface $response
+     * @param string[] $args
+     * @return ResponseInterface
+     * @throws ImANumptyException
+     */
     public function deleteUser(RequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        $response->getBody()->write(json_encode(['message' => '@todo - configure ' . __METHOD__]));
+        $response->getBody()->write(self::jsonEncodeArray(['message' => '@todo - configure ' . __METHOD__]));
         return $response->withStatus(self::ACCEPTED);
     }
 }
