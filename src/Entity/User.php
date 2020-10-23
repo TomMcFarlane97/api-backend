2f667917fde24f747753c38027b87315a1caaafa
Tom McFarlane <tom.mcfarlane@gear4music.com>
2020-10-23 22:20:08 +0100

    Updated the user and added in a todo comment to sort out cammel casing vs snake_casing

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,17 +2,17 @@
 
 namespace App\Entity;
 
+/**
+ * Class User
+ * @package App\Entity
+ * @todo - figure out how to "magically" convert snake case db characters to camelCase
+ */
 class User
 {
     private int $id;
-    private string $firstName;
-    private string $lastName;
-    private string $emailAddress;
-
-    public function __construct(int $id)
-    {
-        $this->id = $id;
-    }
+    private string $first_name;
+    private string $last_name;
+    private string $email_address;
 
     public function getId(): int
     {
@@ -21,32 +21,32 @@ class User
 
     public function get_first_name(): string
     {
-        return $this->firstName;
+        return $this->first_name;
     }
 
     public function set_first_name(string $firstName): void
     {
-        $this->firstName = $firstName;
+        $this->first_name = $firstName;
     }
 
     public function get_last_name(): string
     {
-        return $this->lastName;
+        return $this->last_name;
     }
 
     public function set_last_name(string $lastName): void
     {
-        $this->lastName = $lastName;
+        $this->last_name = $lastName;
     }
 
     public function get_email_address(): string
     {
-        return $this->emailAddress;
+        return $this->email_address;
     }
 
     public function set_email_address(string $emailAddress): void
     {
-        $this->emailAddress = $emailAddress;
+        $this->email_address = $emailAddress;
     }
 
 }
