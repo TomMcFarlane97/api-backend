91f0c33fe66afc4231b07f6bf0d5c54d8a4085e6
Tom McFarlane <tom.mcfarlane@gear4music.com>
2020-10-24 12:52:11 +0100

    Added in service layer to validate and process request

--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -2,14 +2,18 @@
 
 namespace App\Service;
 
+use App\Controller\AbstractController;
 use App\Entity\User;
 use App\Exceptions\DatabaseException;
 use App\Exceptions\RepositoryException;
+use App\Exceptions\RequestException;
+use App\Interfaces\ConvertToArrayInterface;
 use App\Repository\UserRepository;
 
 class UserService
 {
     private UserRepository $userRepository;
+
     public function __construct(UserRepository $userRepository)
     {
         $this->userRepository = $userRepository;
@@ -32,5 +36,78 @@ class UserService
     public function getUserById(int $userId): ?User
     {
         return $this->userRepository->find($userId);
+    }
+
+    /**
+     * @param array<string, mixed> $userBody
+     * @return User
+     * @throws RepositoryException|RequestException
+     */
+    public function createUser(array $userBody): User
+    {
+        return $this->userRepository->createUser($this->mergeUserBodyToObject($userBody));
+    }
+
+    /**
+     * @param array<string, mixed> $userBody
+     * @return User
+     * @throws RepositoryException|RequestException
+     */
+    private function mergeUserBodyToObject(array $userBody): User
+    {
+        $columnSetters = $this->userRepository->getColumnSetters(true);
+        $entity = $this->userRepository->getEntityName();
+        /** @var User $user */
+        $user = new $entity();
+        foreach ($userBody as $columnKey => $columnValue) {
+            $this->throwErrorIfNotValidSetter($columnSetters, $columnKey, $entity, $user);
+            $user->{$columnSetters[$columnKey]}($columnValue);
+            unset($columnSetters[$columnKey]);
+        }
+
+        if (!empty($columnSetters)) {
+            throw new RequestException(
+                sprintf(
+                    'Body of request does not contain the following values %s',
+                    implode(',', array_keys($columnSetters))
+                ),
+                AbstractController::UNPROCESSABLE_ENTITY
+            );
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param string[] $columnSetters
+     * @param string $columnKey
+     * @param string $entityString
+     * @param ConvertToArrayInterface $entityObject
+     * @throws RepositoryException|RequestException
+     */
+    private function throwErrorIfNotValidSetter(
+        array $columnSetters,
+        string $columnKey,
+        string $entityString,
+        ConvertToArrayInterface $entityObject
+    ): void {
+        if (empty($columnSetters[$columnKey])) {
+            throw new RequestException(sprintf(
+                'Key name "%s" is not valid, the only valid key names are: %s',
+                $columnKey,
+                $this->userRepository->getColumnKeysAsString()
+            ));
+        }
+
+        $method = $columnSetters[$columnKey];
+        if (!method_exists($entityObject, $method)) {
+            throw new RepositoryException(
+                sprintf(
+                    'Method "%s" does not exist on entity "%s". Check the set up for its repository.',
+                    $method,
+                    $entityString
+                ),
+                AbstractController::NOT_IMPLEMENTED);
+        }
     }
 }
